75036324df0b3a06a82096601ed08c2a3c93dc08
hidiyatullin <art_78@mail.ru>
2023-02-27 22:51:44 +0300

    Fix10: скорректировал иерархию пакетов

--- main-service/src/main/java/ru/practicum/mainService/MainService.java
+++ main-service/src/main/java/ru/practicum/mainService/MainService.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainService {
+public class MainService{
     public static void main(String[] args) {
         SpringApplication.run(MainService.class);
     }
