2722cd5b9ea2ffa76018193d16aa899a996340a2
hidiyatullin <art_78@mail.ru>
2023-02-27 20:44:27 +0300

    Fix6: не пересобрал jar

--- stats-service2/stats-server/src/main/java/app/StatsServer.java
+++ stats-service2/stats-server/src/main/java/app/StatsServer.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StatsServer {
     public static void main(String[] args) {
         SpringApplication.run(StatsServer.class);
+
     }
 }
